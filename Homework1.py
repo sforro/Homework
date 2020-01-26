55a14b6b48cbe402f7cfcd11dc146e3517f1320c
sforro <59828239+sforro@users.noreply.github.com>
2020-01-27 00:10:25 +0100

    Add files via upload

---
+++ Homework1.py
@@ -0,0 +1,16 @@
+name = input("Enter your name: ")
+print("Hello, " + name)
+
+from math import pi
+
+radius = float(input("Input the radius of the circle: "))
+surface = pi * radius**2
+print(surface)
+
+Number1 = input("Enter the numerator: ")
+Number2 = input("Enter the denominator: ")
+Division = Number1/Number2
+print(round(Division))
+
+
+
