a69a374e6648c2c55163662caa55962a8291213a
sforro <59828239+sforro@users.noreply.github.com>
2020-05-02 23:36:58 +0200

    Update Homework1.py

--- Homework1.py
+++ Homework1.py
@@ -1,16 +1,14 @@
+import math
+
 name = input("Enter your name: ")
 print("Hello, " + name)
 
-from math import pi
-
 radius = float(input("Input the radius of the circle: "))
-surface = pi * radius**2
+surface = math.pi * radius ** 2
 print(surface)
 
 Number1 = input("Enter the numerator: ")
 Number2 = input("Enter the denominator: ")
-Division = Number1/Number2
+
+Division = int(Number1) / int(Number2)
 print(round(Division))
-
-
-
